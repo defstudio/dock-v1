ad63dd11e487f652d011b3ec2d46ae63304c56ad
Fabio Ivona <fabio.ivona@defstudio.it>
2020-09-07 16:56:33 +0200

    set redis password
    /spend 4m

--- app/Recipes/Laravel/Containers/EchoServer.php
+++ app/Recipes/Laravel/Containers/EchoServer.php
@@ -16,58 +16,57 @@
                 'context' => 'https://gitlab.com/defstudio/docker/laravel-echo-server.git',
             ],
             'environment' => [
-                'ECHO_AUTH_HOST'=>'http://nginx',
-                'ECHO_DEBUG'=>'false',
-                'ECHO_CLIENTS'=>[],
-                'ECHO_REDIS_PORT'=>6379,
-                'ECHO_REDIS_HOSTNAME'=>'redis',
-                'ECHO_REDIS_PASSWORD'=>'',
-                'ECHO_DEVMODE'=>'false',
-                'ECHO_PROTOCOL'=>'http',
-                'ECHO_SSL_CERT_PATH'=>'',
-                'ECHO_SSL_KEY_PATH'=>'',
-                'ECHO_SSL_CHAIN_PATH'=>'',
-                'ECHO_SSL_PASSPHRASE'=>'',
-                'ECHO_ALLOW_CORS'=>'true',
-                'ECHO_ALLOW_ORIGIN'=>'http://localhost:80',
-                'ECHO_ALLOW_METHODS'=>'"GET, POST"',
-                'ECHO_ALLOW_HEADERS'=>'"Origin, Content-Type, X-Auth-Token, X-Requested-With, Accept, Authorization, X-CSRF-TOKEN, X-Socket-Id"',
+                'ECHO_AUTH_HOST=http://nginx',
+                'ECHO_DEBUG=false',
+                'ECHO_CLIENTS=[]',
+                'ECHO_REDIS_PORT=6379',
+                'ECHO_REDIS_HOSTNAME=redis',
+                'ECHO_REDIS_PASSWORD=',
+                'ECHO_DEVMODE=false',
+                'ECHO_PROTOCOL=http',
+                'ECHO_SSL_CERT_PATH=',
+                'ECHO_SSL_KEY_PATH=',
+                'ECHO_SSL_CHAIN_PATH=',
+                'ECHO_SSL_PASSPHRASE=',
+                'ECHO_ALLOW_CORS=true',
+                'ECHO_ALLOW_ORIGIN=http://localhost:80',
+                'ECHO_ALLOW_METHODS="GET, POST"',
+                'ECHO_ALLOW_HEADERS="Origin, Content-Type, X-Auth-Token, X-Requested-With, Accept, Authorization, X-CSRF-TOKEN, X-Socket-Id"',
             ],
             'expose'      => [6001],
         ];
 
 
-
         public function set_auth_host($service_name = "nginx"){
-            $this->set_environment('ECHO_AUTH_HOST', "http://$service_name");
+            $this->set_environment('ECHO_AUTH_HOST', "http://$service_name", false);
         }
 
         public function set_debug(bool $enabled = false){
             if($enabled){
-                $this->set_environment("ECHO_DEBUG", "true");
+                $this->set_environment("ECHO_DEBUG", "true", false);
             } else{
-                $this->set_environment("ECHO_DEBUG", "false");
+                $this->set_environment("ECHO_DEBUG", "false", false);
             }
         }
 
         public function set_devmode(bool $enabled = false){
             if($enabled){
-                $this->set_environment("ECHO_DEVMODE", "true");
+                $this->set_environment("ECHO_DEVMODE", "true", false);
             } else{
-                $this->set_environment("ECHO_DEVMODE", "false");
+                $this->set_environment("ECHO_DEVMODE", "false", false);
             }
         }
 
         public function set_allow_cors(bool $enabled = false){
             if($enabled){
-                $this->set_environment("ECHO_ALLOW_CORS", "true");
+                $this->set_environment("ECHO_ALLOW_CORS", "true", false);
             } else{
-                $this->set_environment("ECHO_ALLOW_CORS", "false");
+                $this->set_environment("ECHO_ALLOW_CORS", "false", false);
             }
         }
 
         public function set_allow_origin(string $url = "http://localhost:80"){
-            $this->set_environment("ECHO_ALLOW_ORIGIN", $url);
+            $this->set_environment("ECHO_ALLOW_ORIGIN", $url, false);
         }
 
         /**
@@ -76,7 +75,7 @@
         public function set_allow_methods($methods = ["GET", "POST"]){
             $methods = Arr::wrap($methods);
             $methods = implode(',', $methods);
-            $this->set_environment("ECHO_ALLOW_HEADERS", "$methods");
+            $this->set_environment("ECHO_ALLOW_HEADERS", "$methods", false);
         }
 
 
@@ -86,7 +85,7 @@
         public function set_allow_headers($headers = ["Origin", "Content-Type", "X-Auth-Token", "X-Requested-With", "Accept", "Authorization", "X-CSRF-TOKEN", "X-Socket-Id"]){
             $headers = Arr::wrap($headers);
             $headers = implode(',', $headers);
-            $this->set_environment("ECHO_ALLOW_METHODS", "$headers");
+            $this->set_environment("ECHO_ALLOW_METHODS", "$headers", false);
         }
 
 
@@ -95,39 +94,40 @@
          */
         public function set_clients($clients = []){
             $clients = Arr::wrap($clients);
-            $this->set_environment("ECHO_CLIENTS", $clients);
+            $clients = implode(',', $clients);
+            $this->set_environment("ECHO_CLIENTS", "[$clients]", false);
         }
 
         public function set_redis_port($port = "6379"){
-            $this->set_environment("ECHO_REDIS_PORT", $port);
+            $this->set_environment("ECHO_REDIS_PORT", $port, false);
         }
 
         public function set_redis_service($service_name = "redis"){
-            $this->set_environment("ECHO_REDIS_HOSTNAME", $service_name);
+            $this->set_environment("ECHO_REDIS_HOSTNAME", $service_name, false);
         }
 
-        public function set_redis_password($password = ""){
-            $this->set_environment("ECHO_REDIS_PASSWORD", $password);
+        public function set_redis_password($password = "redis"){
+            $this->set_environment("ECHO_REDIS_PASSWORD", $password, false);
         }
 
         public function set_protocol($protocol = "http"){
-            $this->set_environment("ECHO_PROTOCOL", $protocol);
+            $this->set_environment("ECHO_PROTOCOL", $protocol, false);
         }
 
         public function set_ssl_cert_path($path = ""){
-            $this->set_environment("ECHO_SSL_CERT_PATH", $path);
+            $this->set_environment("ECHO_SSL_CERT_PATH", $path, false);
         }
 
         public function set_ssl_key_path($path = ""){
-            $this->set_environment("ECHO_SSL_KEY_PATH", $path);
+            $this->set_environment("ECHO_SSL_KEY_PATH", $path, false);
         }
 
         public function set_ssl_chain_path($path = ""){
-            $this->set_environment("ECHO_SSL_CHAIN_PATH", $path);
+            $this->set_environment("ECHO_SSL_CHAIN_PATH", $path, false);
         }
 
         public function set_ssl_passphrase($passhprase = ""){
-            $this->set_environment("ECHO_SSL_PASSPHRASE", $passhprase);
+            $this->set_environment("ECHO_SSL_PASSPHRASE", $passhprase, false);
         }
 
 
