3262ce213326320a0aea9bbc9a434fb88a37b833
Fabio Ivona <fabio.ivona@defstudio.it>
2021-01-09 09:03:27 +0100

    wip update strategy

--- app/Updater/GithubStrategy.php
+++ app/Updater/GithubStrategy.php
@@ -8,4 +8,5 @@ use LaravelZero\Framework\Components\Updater\Strategy\StrategyInterface;
 final class GithubStrategy extends \Humbug\SelfUpdate\Strategy\GithubStrategy implements StrategyInterface
 {
 
+
 }
