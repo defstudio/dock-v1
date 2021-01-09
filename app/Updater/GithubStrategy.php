2f395537ec0efb0af7b1cec74f23c37bc3428b0c
Fabio Ivona <fabio.ivona@defstudio.it>
2021-01-09 09:42:30 +0100

    fix updater versioning

--- app/Updater/GithubStrategy.php
+++ app/Updater/GithubStrategy.php
@@ -3,27 +3,29 @@
 
 namespace App\Updater;
 
+use Humbug\SelfUpdate\Exception\JsonParsingException;
 use Humbug\SelfUpdate\Updater;
+use Humbug\SelfUpdate\VersionParser;
 use LaravelZero\Framework\Components\Updater\Strategy\StrategyInterface;
 use Str;
 
 final class GithubStrategy extends \Humbug\SelfUpdate\Strategy\GithubStrategy implements StrategyInterface
 {
-    public function getCurrentLocalVersion(Updater $updater)
+    public function getCurrentLocalVersion(Updater $updater): string
     {
         $version = parent::getCurrentLocalVersion($updater);
-        dump("Local version: $version");
+        echo "Local version: $version";
         return $version;
     }
 
-    public function getCurrentRemoteVersion(Updater $updater)
+    public function getCurrentRemoteVersion(Updater $updater): string
     {
         $version = parent::getCurrentRemoteVersion($updater);
-        dump("Remote version: $version");
+        echo "Remote version: $version";
         return $version;
     }
 
-    protected function getDownloadUrl(array $package)
+    protected function getDownloadUrl(array $package): string
     {
         $url = parent::getDownloadUrl($package);
         return Str::of($url)->append('dock');
