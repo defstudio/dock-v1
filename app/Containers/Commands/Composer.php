aac5180dc47c3b1e10fc1fc9caf464e3fd89323a
Fabio Ivona <fabio.ivona@defstudio.it>
2021-01-09 09:58:19 +0100

    fix node arguments and options

--- app/Containers/Commands/Composer.php
+++ app/Containers/Commands/Composer.php
@@ -1,55 +1,46 @@
-<?php
+<?php /** @noinspection PhpUnhandledExceptionInspection */
+
+/** @noinspection PhpMissingFieldTypeInspection */
 
 
-    namespace App\Containers\Commands;
+namespace App\Containers\Commands;
 
-    use App\Exceptions\DockerServiceNotFoundException;
-    use App\Services\DockerService;
-    use App\Services\TerminalService;
-    use Illuminate\Contracts\Container\BindingResolutionException;
-    use LaravelZero\Framework\Commands\Command;
+use App\Services\DockerService;
+use App\Services\TerminalService;
+use LaravelZero\Framework\Commands\Command;
 
-    class Composer extends Command{
-        protected $signature = 'composer';
+class Composer extends Command
+{
+    protected $signature = 'composer';
 
-        protected $description = 'Runs a composer command';
+    protected $description = 'Runs a composer command';
 
-        public function __construct()
-        {
-            parent::__construct();
-            $this->ignoreValidationErrors();
-        }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->ignoreValidationErrors();
+    }
 
-        /**
-         * @param DockerService   $docker_service
-         * @param TerminalService $terminal
-         *
-         * @return int
-         * @throws DockerServiceNotFoundException
-         * @throws BindingResolutionException
-         */
-        public function handle(DockerService $docker_service, TerminalService $terminal){
+    public function handle(DockerService $docker_service, TerminalService $terminal): int
+    {
+        $terminal->init($this->output);
 
-            $terminal->init($this->output);
+        $arguments = (string) $this->input;
 
-            $arguments = (string) $this->input;
+        if (empty($arguments)) {
+            $this->info('Log into Composer Shell');
 
-            if(empty($arguments)){
-                $this->info('Log into Composer Shell');
-
-                return $terminal->execute([
-                    'docker-compose',
-                    'run',
-                    '--rm',
-                    'composer',
-                    'bash',
-                ]);
-            } else{
-                $composer_commands = explode(' ', $arguments);
-                $commands = array_merge(['composer'], $composer_commands);
-                return $docker_service->service('composer')->run($terminal, $commands);
-            }
-
-
+            return $terminal->execute([
+                'docker-compose',
+                'run',
+                '--rm',
+                'composer',
+                'bash',
+            ]);
+        } else {
+            $composer_commands = explode(' ', $arguments);
+            $commands = array_merge(['composer'], $composer_commands);
+            return $docker_service->service('composer')->run($terminal, $commands);
         }
     }
+}
--- app/Containers/Commands/Node.php
+++ app/Containers/Commands/Node.php
@@ -1,41 +1,45 @@
-<?php
+<?php /** @noinspection PhpUnhandledExceptionInspection */
+
+/** @noinspection PhpMissingFieldTypeInspection */
 
 
-    namespace App\Containers\Commands;
+namespace App\Containers\Commands;
 
-    use App\Exceptions\DockerServiceNotFoundException;
     use App\Services\DockerService;
     use App\Services\TerminalService;
-    use Illuminate\Contracts\Container\BindingResolutionException;
     use LaravelZero\Framework\Commands\Command;
 
     class Node extends Command{
-        protected $signature = 'node
-                                {commands* : node commands to execute} ';
+        protected $signature = 'node';
 
-        protected $description = 'Executes a node command';
+        protected $description = 'Runs a node command';
 
-        /**
-         * @param DockerService $docker_service
-         * @param TerminalService $terminal
-         * @return int
-         * @throws DockerServiceNotFoundException
-         * @throws BindingResolutionException
-         */
-        public function handle(DockerService $docker_service, TerminalService $terminal){
+        public function __construct()
+        {
+            parent::__construct();
+            $this->ignoreValidationErrors();
+        }
 
+        public function handle(DockerService $docker_service, TerminalService $terminal): int
+        {
             $terminal->init($this->output);
 
-            $commands = [
-                "node",
-            ];
+            $arguments = (string) $this->input;
 
-            $composer_commands = $this->argument("commands");
-            if(!empty($composer_commands)){
-                $commands = array_merge($commands, $composer_commands);
+            if (empty($arguments)) {
+                $this->info('Log into Node Shell');
+
+                return $terminal->execute([
+                    'docker-compose',
+                    'run',
+                    '--rm',
+                    'node',
+                    'bash',
+                ]);
+            } else {
+                $node_commands = explode(' ', $arguments);
+                $commands = array_merge(['node'], $node_commands);
+                return $docker_service->service('node')->run($terminal, $commands);
             }
-            return $docker_service->service('node')->run($terminal, $commands);
-
-
         }
     }
