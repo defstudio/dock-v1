16e7e729ffc10e5fe83cdf11a85fc08ee123d670
Fabio Ivona <fabio.ivona@defstudio.it>
2021-01-30 13:35:40 +0100

    show local version in updater

--- app/Updater/GithubStrategy.php
+++ app/Updater/GithubStrategy.php
@@ -3,11 +3,42 @@
 
 namespace App\Updater;
 
+use Humbug\SelfUpdate\Exception\JsonParsingException;
+use Humbug\SelfUpdate\Updater;
+use Humbug\SelfUpdate\VersionParser;
 use Illuminate\Support\Str;
 use LaravelZero\Framework\Components\Updater\Strategy\StrategyInterface;
 
 final class GithubStrategy extends \Humbug\SelfUpdate\Strategy\GithubStrategy implements StrategyInterface
 {
+    public function getCurrentRemoteVersion(Updater $updater)
+    {
+        /** Switch remote request errors to HttpRequestExceptions */
+        set_error_handler(array($updater, 'throwHttpRequestException'));
+        $packageUrl = $this->getApiUrl();
+        $package = json_decode(humbug_get_contents($packageUrl), true);
+        restore_error_handler();
+
+        if (null === $package || json_last_error() !== JSON_ERROR_NONE) {
+            throw new JsonParsingException(
+                'Error parsing JSON package data'
+                . (function_exists('json_last_error_msg') ? ': ' . json_last_error_msg() : '')
+            );
+        }
+
+        $versions = array_keys($package['packages'][$this->getPackageName()]);
+        $versionParser = new VersionParser($versions);
+        if ($this->getStability() === self::STABLE) {
+            echo 'remote version: ' .  $versionParser->getMostRecentStable();
+        } elseif ($this->getStability() === self::UNSTABLE) {
+            echo 'remote version: ' . $versionParser->getMostRecentUnstable();
+        } else {
+            echo 'remote version: ' . $versionParser->getMostRecentAll();
+        }
+
+        return parent::getCurrentRemoteVersion($updater);
+    }
+
     protected function getDownloadUrl(array $package): string
     {
         $url = parent::getDownloadUrl($package);
