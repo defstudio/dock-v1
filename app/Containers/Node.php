8fe3733effa0127c486ef8b96b21165b83fa6421
Fabio Ivona <fabio.ivona@defstudio.it>
2021-04-10 09:44:59 +0200

    update node image

--- app/Containers/Node.php
+++ app/Containers/Node.php
@@ -1,32 +1,33 @@
-<?php /** @noinspection LaravelFunctionsInspection */
+<?php
+/** @noinspection LaravelFunctionsInspection */
 
 
 namespace App\Containers;
 
+class Node extends Container
+{
+    protected string $service_name = "node";
 
-	use App\Exceptions\ContainerException;
+    protected array $service_definition = [
+        'working_dir' => '/var/www',
+        'image'       => 'defstudio/node:alpine-lts',
+    ];
 
-    class Node extends Container{
-        protected string $service_name = "node";
+    protected array $volumes = [
+        self::HOST_SRC_VOLUME_PATH => '/var/www',
+    ];
 
-        protected array $service_definition = [
-            'working_dir' => '/var/www',
-            'image'       => 'defstudio/node:alpine-lts',
-        ];
-
-        protected array $volumes = [
-            self::HOST_SRC_VOLUME_PATH => '/var/www'
-        ];
-
-        public function __construct(){
-            parent::__construct();
-            $this->set_user_uid(env('USER_ID'));
-        }
-
-        public function commands(): array{
-           return [
-               Commands\Node::class,
-               Commands\Npm::class,
-           ];
-        }
+    public function __construct()
+    {
+        parent::__construct();
+        $this->set_user_uid(env('USER_ID'));
     }
+
+    public function commands(): array
+    {
+        return [
+            Commands\Node::class,
+            Commands\Npm::class,
+        ];
+    }
+}
