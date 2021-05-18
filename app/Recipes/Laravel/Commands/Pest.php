27c05e2f538e6503936e1fc7e000ef91d2c34073
Fabio Ivona <fabio.ivona@defstudio.it>
2021-05-18 22:13:49 +0200

    adds pest command to laravel recipe

---
+++ app/Recipes/Laravel/Commands/Pest.php
@@ -0,0 +1,36 @@
+<?php
+
+
+    namespace App\Recipes\Laravel\Commands;
+
+
+    use App\Exceptions\DockerServiceNotFoundException;
+    use App\Services\DockerService;
+    use App\Services\TerminalService;
+    use Illuminate\Contracts\Container\BindingResolutionException;
+    use LaravelZero\Framework\Commands\Command;
+
+    class Pest extends Command{
+        protected $signature = 'pest';
+
+        protected $description = 'Starts Laravel tests with Pest';
+
+        /**
+         * @param DockerService $docker_service
+         * @param TerminalService $terminal
+         *
+         * @return int
+         * @throws DockerServiceNotFoundException
+         * @throws BindingResolutionException
+         */
+        public function handle(DockerService $docker_service, TerminalService $terminal){
+
+            $terminal->init($this->output);
+
+            $commands = array_merge([
+                'php',
+                './vendor/bin/pest',
+            ]);
+            return $docker_service->service('php')->run($terminal, $commands);
+        }
+    }
