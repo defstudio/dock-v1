60f4b33d108764389900c47360837e57214323b1
Fabio Ivona <fabio.ivona@defstudio.it>
2022-03-30 11:53:42 +0200

    Update Start.php

--- app/Commands/Start.php
+++ app/Commands/Start.php
@@ -40,6 +40,7 @@
 
             $this->task('Starting containers', function() use ($terminal){
                 $command = [
+                    'COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1',
                     'docker-compose',
                     'up',
                     '-d',
