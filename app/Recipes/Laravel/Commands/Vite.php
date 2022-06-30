c27ae4352132e3ca72c4d411efe12f56980411e5
Fabio Ivona <fabio.ivona@defstudio.it>
2022-06-30 20:42:01 +0200

    add vite command
    Took 6 minutes

---
+++ app/Recipes/Laravel/Commands/Vite.php
@@ -0,0 +1,42 @@
+<?php
+
+
+    namespace App\Recipes\Laravel\Commands;
+
+    use App\Exceptions\DockerServiceNotFoundException;
+    use App\Services\DockerService;
+    use App\Services\TerminalService;
+    use Illuminate\Contracts\Container\BindingResolutionException;
+    use LaravelZero\Framework\Commands\Command;
+
+    class Vite extends Command{
+        protected $signature = 'vite';
+
+        protected $description = 'Launch vite server';
+
+        /**
+         * @param DockerService $docker_service
+         * @param TerminalService $terminal
+         * @return int
+         * @throws DockerServiceNotFoundException
+         * @throws BindingResolutionException
+         */
+        public function handle(DockerService $docker_service, TerminalService $terminal){
+
+            $terminal->init($this->output);
+
+            $commands = [
+                "docker",
+                "run",
+                "--rm",
+                "-workdir /var/www",
+                '--volume $PWD/src:/var/www',
+                '--publish 127.0.0.1:3000:3000',
+                'defstudio/node:alpine-lts',
+                'npm run dev',
+            ];
+
+
+            return $terminal->execute($commands);
+        }
+    }
