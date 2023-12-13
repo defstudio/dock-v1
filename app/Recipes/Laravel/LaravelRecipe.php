4a6398c72954857c139c3eeca7e8d0b81572b0a0
Fabio Ivona <fabio.ivona@defstudio.it>
2023-12-13 18:47:44 +0100

    fix cache with livewire

--- app/Recipes/Laravel/LaravelRecipe.php
+++ app/Recipes/Laravel/LaravelRecipe.php
@@ -291,7 +291,14 @@ class LaravelRecipe extends DockerComposeRecipe
         ";
 
         if(($cache_age = env('NGINX_CACHE_AGE')) && ($cache_files = env('NGINX_CACHE_FILES'))){
+
+
+
             $extra_site_config .= "
+                location ^~ /livewire {
+                    try_files \$uri \$uri/ /index.php?\$query_string;
+                }
+
                 location ~* \.($cache_files)$ {
                     expires $cache_age;
                     etag off;
